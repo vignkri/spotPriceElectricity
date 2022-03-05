727e116b7cd5fcb76fd987a23b33b908ddc49765
Vignesh Krishnamoorthy <vignesh@hybridgreentech.com>
2022-03-05 16:59:04 +0100

    add function to get spot price data from energidataservice

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,37 @@
 <script lang="ts">
 	export let name: string;
 
+	async function postToEnergidataservice() {
+
+		let url: string = 'https://data-api.energidataservice.dk/v1/graphql';
+
+		// Get the data for given date
+		const today: Date = new Date();
+		let days: number = 86400000 //number of milliseconds in a day
+		const seven_days_ago: Date = new Date( today.getTime() - (7 * days));
+
+
+		let query = {
+			"query": "query Dataset {elspotprices(where: {HourUTC: {_gte: \"2022-02-27\", _lt: \"2022-03-05\"}PriceArea: {_eq: \"DK2\"}} order_by: {HourUTC: desc} offset: 0){HourUTC HourDK PriceArea SpotPriceDKK SpotPriceEUR }}"
+		};
+
+		const response = await fetch(url, {
+			method: "POST",
+			mode: "cors",
+			cache: "no-cache",
+			credentials: "same-origin",
+			headers: {
+				'Content-Type': "application/json",
+			},
+			body: JSON.stringify(query)
+		});
+
+		return await response.json
+	}
+
+	postToEnergidataservice()
+		.then(data => {console.log(data)});
+
 	const fetchImage = (async () => {
 		const response = await fetch('https://dog.ceo/api/breeds/image/random')
     return await response.json()
