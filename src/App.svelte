90e990e603156fb4c42f33c3bb2f4bc424788146
Vignesh Krishnamoorthy <vignesh@hybridgreentech.com>
2022-03-05 16:21:58 +0100

    add placeholder for dog images

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
 	export let name: string;
+
+	const fetchImage = (async () => {
+		const response = await fetch('https://dog.ceo/api/breeds/image/random')
+    return await response.json()
+	})()
 </script>
 
 <main>
 	<h1>{name}</h1>
+	{#await fetchImage}
+		<p>...waiting</p>
+	{:then dog_data }
+		<!-- svelte-ignore a11y-img-redundant-alt -->
+		<img src={dog_data.message} alt="Dog image" />
+	{:catch error}
+		<p>An error occurred!</p>
+	{/await}
 </main>
 
 <style>
