0e6e1d8fd2f51fc501e5b0f1e59a9cedc6e75902
Vignesh Krishnamoorthy <vignesh@hybridgreentech.com>
2022-03-06 16:22:44 +0100

    add function to draw timeseries

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 
 	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+import { svg_element } from 'svelte/internal';
 
 	export let name: string;
-
+ 
 	type Record = {
 			HourUTC: Date;
 			HourDK: Date;
@@ -52,10 +54,11 @@
 		return await data_array
 	}
 
-	const elspot_query: Promise<string> = (async() => {
+	let priceAreaFilter: string = "DK2";
 
-		const filter: string = "DK2";
-		const response = await postToEnergidataservice(filter);
+	const elspot_query = (async() => {
+
+		const response = await postToEnergidataservice(priceAreaFilter);
 
 		const output = response.map(record => {
 
@@ -63,14 +66,92 @@
 			let spotPriceInDKKForkWH: number = record.SpotPriceDKK / 1000;
 
 			return {
-				isoDate: current_date.toISOString().split("T")[0],
-				hourOfDay: current_date.toISOString().split("T")[1].split("+")[0],
-				spotPriceDKKkWh: spotPriceInDKKForkWH,
+				dates: current_date,
+				spotPriceDKK: spotPriceInDKKForkWH,
 			}
 		})
 		
-		return await JSON.stringify(output)
+		return await output
 	})();
+
+
+	let el;
+
+	onMount(async() => {
+
+		let data = await elspot_query;
+
+		console.log(data);
+
+		const X = d3.map(data, d => d.dates);
+		const Y = d3.map(data, d => d.spotPriceDKK);
+		const I = d3.range(X.length);
+		var defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
+		const D = d3.map(data, defined);
+
+		// Compute domains
+		const xDomain = d3.extent(X);
+		const yDomain = [0, d3.max(Y)];
+
+		let marginDef = {
+			marginTop: 20,
+			marginRight: 30,
+			marginBottom: 30,
+			marginLeft: 40
+		};
+
+		const width = 800;
+		const height = 400;
+
+		const xRange = [marginDef.marginLeft, width - marginDef.marginRight];
+		const yRange = [height - marginDef.marginBottom, marginDef.marginTop];
+
+		const xScale = d3.scaleUtc(xDomain, xRange);
+		const yScale = d3.scaleLinear(yDomain, yRange);
+		const xAxis = d3.axisBottom(xScale).ticks(width / 80).tickSizeOuter(0);
+		const yAxis = d3.axisLeft(yScale).ticks(height / 40);
+
+		// Construct a line generator
+		const line = d3.line()
+        .defined(i => D[i])
+        .curve(d3.curveStep)
+        .x(i => xScale(X[i]))
+        .y(i => yScale(Y[i]));
+
+
+		const svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+  
+		svg.append("g")
+			.attr("transform", `translate(0,${height - marginDef.marginBottom})`)
+			.call(xAxis);
+	
+		svg.append("g")
+			.attr("transform", `translate(${marginDef.marginLeft},0)`)
+			.call(yAxis)
+			.call(g => g.select(".domain").remove())
+			.call(g => g.selectAll(".tick line").clone()
+				.attr("x2", width - marginDef.marginLeft - marginDef.marginRight)
+				.attr("stroke-opacity", 0.1))
+			.call(g => g.append("text")
+				.attr("x", -marginDef.marginLeft)
+				.attr("y", 10)
+				.attr("fill", "currentColor")
+				.attr("text-anchor", "start")
+				.text("Spot Price DKK/kWh"));
+	
+		svg.append("path")
+			.attr("fill", "none")
+			.attr("stroke", "currentColor")
+			.attr("stroke-width", 1.5)
+			.attr("stroke-linecap", "round")
+			.attr("stroke-linejoin", "round")
+			.attr("stroke-opacity", 1)
+			.attr("d", line(I));
+	});
 
 </script>
 
@@ -82,13 +163,10 @@
 		</p>
 	</div>
 	<hr>
-	{#await elspot_query}
-		<p>...waiting</p>
-	{:then timeseries }
-		<p>{timeseries}</p>
-	{:catch error}
-		<p>An error occurred, unable to fetch data</p>
-	{/await}
+	<svg></svg>
+	<div bind:this={el} id="timeseries" class="svg">
+
+	</div>
 </main>
 
 <style>
